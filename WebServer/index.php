5f608b3467750b405bc0a1a2ddb894a09e891d56
Akhil Ramachandran <akhilram@usc.edu>
2015-06-27 15:39:12 -0700

    parameter change

--- WebServer/index.php
+++ WebServer/index.php
@@ -2,12 +2,12 @@
 
 $category = $_GET["category"];
 switch ($category) {
-	case 1:
+	case "topStories":
 	// top stories
 	$query = 'http://api.nytimes.com/svc/topstories/v1/home.json?api-key=31f908317e61cdf63f264bb88b75881c:14:72395557';
 	break;
 	
-	case 2:
+	case "mostPopular":
 	// most popular
 	$query = 'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=c56c29e61017b4bc452a26d1d20a7f95:13:72395557';
 	break;
