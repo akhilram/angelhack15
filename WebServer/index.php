2b00d5355b8e63b07b18fd6f0409d499c212da25
Akhil Ramachandran <akhilram@usc.edu>
2015-06-27 20:58:13 -0700

    pagination for most popular done

--- WebServer/index.php
+++ WebServer/index.php
@@ -2,49 +2,56 @@
 
 function financeAPI() {
 
-    $query = 'http://pipes.yahoo.com/pipes/pipe.run?_id=2FV68p9G3BGVbc7IdLq02Q&_render=json&feedcount=10&feedurl=http%3A%2F%2Ffinance.yahoo.com%2Frss%2Fheadline%3Fs%3Dyhoo';
+	$query = 'http://pipes.yahoo.com/pipes/pipe.run?_id=2FV68p9G3BGVbc7IdLq02Q&_render=json&feedcount=10&feedurl=http%3A%2F%2Ffinance.yahoo.com%2Frss%2Fheadline%3Fs%3Dyhoo';
 
-    $result = file_get_contents($query);
-    $json_result = json_decode($result);
-    $items = $json_result->value->items;
+	$result = file_get_contents($query);
+	$json_result = json_decode($result);
+	$items = $json_result->value->items;
 
-    $feeds = [];
-    foreach ($items as $item) {
-        $feeds[] = $item->title; 
-    }
-    echo json_encode($feeds);
+	$feeds = [];
+	foreach ($items as $item) {
+		$feeds[] = $item->title; 
+	}
+	echo json_encode($feeds);
 }
 
 if (isset($_GET['category']))
-    $category = $_GET["category"];
+	$category = $_GET["category"];
 else
-    $category = "";
+	$category = "";
 
 switch ($category) {
-    case "topStories":
+	case "topStories":
     // top stories
-    $query = 'http://api.nytimes.com/svc/topstories/v1/home.json?api-key=31f908317e61cdf63f264bb88b75881c:14:72395557';
-    break;
+	$query = 'http://api.nytimes.com/svc/topstories/v1/home.json?api-key=31f908317e61cdf63f264bb88b75881c:14:72395557';
+	break;
 
-    case "mostPopular":
+	case "mostPopular":
     // most popular
-    $query = 'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=c56c29e61017b4bc452a26d1d20a7f95:13:72395557';
-    break;
+	$query = 'http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/1.json?api-key=c56c29e61017b4bc452a26d1d20a7f95:13:72395557';
+	if (isset($_GET['page']))
+		$page = intval($_GET['page']);
+	else
+		$page = 1;
+	$page = ($page-1)*20;
+	$page = (string) $page;
+	$query .= '&offset=' . $page;
+	break;
 
-    case "twitter":
+	case "twitter":
     // twiiter
-    include_once('twitter.php');
-    return;
+	include_once('twitter.php');
+	return;
 
-    case "finance":
+	case "finance":
 	//finance
-    financeAPI();
-    return;
+	financeAPI();
+	return;
 
-    default:
+	default:
 	// top stories
-    $query = 'http://api.nytimes.com/svc/topstories/v1/home.json?api-key=31f908317e61cdf63f264bb88b75881c:14:72395557';
-    break;
+	$query = 'http://api.nytimes.com/svc/topstories/v1/home.json?api-key=31f908317e61cdf63f264bb88b75881c:14:72395557';
+	break;
 
 }
 $result = file_get_contents($query);
@@ -54,7 +61,7 @@ $results = $json_result->results;
 
 $feeds = [];
 foreach ($results as $result) {
-    $feeds[] = $result->title;
+	$feeds[] = $result->title;
 }
 
 echo json_encode($feeds);
